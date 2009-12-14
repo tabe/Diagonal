cdefdbdd59980555c07f831945ea1168301df0fa
Takeshi Abe <tabe@fixedpoint.jp>
2009-12-14 15:13:29 +0900

    test to decode an empty file

--- test/vcdiff/diag_vcdiff_decode.c
+++ test/vcdiff/diag_vcdiff_decode.c
@@ -20,20 +20,44 @@ decode3(const char *source, const char *input, const char *output)
 	context = diag_vcdiff_context_new_path(input);
 	context->compatibility = 1;
 	vcdiff = diag_vcdiff_read(context);
-	if (!vcdiff) exit(EXIT_FAILURE);
+	if (!vcdiff) {
+		printf("failed to read\n");
+		exit(EXIT_FAILURE);
+	}
 	vm = diag_vcdiff_vm_new_path(source);
-	if (!vm) exit(EXIT_FAILURE);
-	if (!diag_vcdiff_decode(vm, vcdiff)) exit(EXIT_FAILURE);
+	if (!vm) {
+		printf("failed to create vm with %s\n", source);
+		exit(EXIT_FAILURE);
+	}
+	if (!diag_vcdiff_decode(vm, vcdiff)) {
+		printf("failed to decode\n");
+		exit(EXIT_FAILURE);
+	}
 	fd = open(output, O_RDONLY);
-	if (fd < 0) exit(EXIT_FAILURE);
+	if (fd < 0) {
+		printf("failed to open %s\n", output);
+		exit(EXIT_FAILURE);
+	}
 	r = fstat(fd, &st);
-	if (r < 0) exit(EXIT_FAILURE);
-	if (vm->s_target != (uint32_t)st.st_size) exit(EXIT_FAILURE);
+	if (r < 0) {
+		printf("failed to stat %s\n", output);
+		exit(EXIT_FAILURE);
+	}
+	if (vm->s_target != (uint32_t)st.st_size) {
+		printf("mismatch found: expected size %d, but s_target %d\n", (uint32_t)st.st_size, vm->s_target);
+		exit(EXIT_FAILURE);
+	}
 	expected = (uint8_t *)mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
 	close(fd);
-	if (!expected) exit(EXIT_FAILURE);
+	if (!expected) {
+		printf("failed to mmap %s\n", output);
+		exit(EXIT_FAILURE);
+	}
 	for (i = 0; i < vm->s_target; i++) {
-		if (vm->target[i] != expected[i]) exit(EXIT_FAILURE);
+		if (vm->target[i] != expected[i]) {
+			printf("mismatch found: %c expected, but %c\n", expected[i], vm->target[i]);
+			exit(EXIT_FAILURE);
+		}
 	}
 	munmap(expected, st.st_size);
 	diag_vcdiff_vm_destroy(vm);
@@ -80,6 +104,7 @@ fail_to_decode(const char *input)
 int
 main()
 {
+	DECODE2(empty.vcdiff, empty);
 	DECODE2(hello.vcdiff, hello);
 	DECODE3(rfc3284.txt, rfc3284.txt.vcdiff.1, rfc3284.txt);
 	DECODE3(rfc3284.txt.head, rfc3284.txt.vcdiff.2, rfc3284.txt);
